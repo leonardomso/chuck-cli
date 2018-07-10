1d32bb7f8f85dd5de141ad4f160af41a83be917b
leonardomso <leonardomso11@gmail.com>
2018-07-10 11:12:47 -0300

    finally, good!

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const yargs = require("yargs");
-const chalk = require("chalk");
-const figlet = require("figlet");
-const inquirer = require("inquirer");
-const fetch = require("node-fetch");
+const yargs = require('yargs');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+const fetch = require('node-fetch');
 
-const req = require("./src/requests.js");
+const req = require('./src/requests.js');
 
 const argv = yargs.argv;
 const command = process.argv[2];
@@ -12,34 +12,34 @@ const command = process.argv[2];
 module.exports = () => {
     console.log(
         chalk.green(
-            figlet.textSync("chuck jokes\n", { horizontalLayout: "full" })
+            figlet.textSync('chuck jokes\n', { horizontalLayout: 'full' })
         )
     );
 
     const categories = [
-        "explicit",
-        "dev",
-        "movie",
-        "food",
-        "celebrity",
-        "science",
-        "sport",
-        "political",
-        "religion",
-        "animal",
-        "history",
-        "music",
-        "travel",
-        "career",
-        "money",
-        "fashion"
+        'explicit',
+        'dev',
+        'movie',
+        'food',
+        'celebrity',
+        'science',
+        'sport',
+        'political',
+        'religion',
+        'animal',
+        'history',
+        'music',
+        'travel',
+        'career',
+        'money',
+        'fashion'
     ];
 
     switch (command) {
-        case "joke":
+        case 'joke':
             req.requestJoke();
             break;
-        case "categories":
+        case 'categories':
             req.requestCategories();
             break;
         default:
